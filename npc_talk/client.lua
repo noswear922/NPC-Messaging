711e8b74e055d272a6ad838ca1f5338c22b4e524
noswear922 <98922101+noswear922@users.noreply.github.com>
2024-09-12 09:03:54 +0300

    Add files via upload

--- npc_talk/client.lua
+++ npc_talk/client.lua
@@ -1,38 +1,39 @@
+QBCore = exports['qb-core']:GetCoreObject()
 local npcs = {
     {
         model = "a_f_y_business_01", -- Ped model
         coords = vector4(-557.01, -186.30, 38.22, 215.14), -- Ped coords
-        webhook = "https://discord.com/api/webhooks/1283480789324926976/F0BbluHQ_5Qr3ZmPCt7mtR9Ff6PuBFGjG0E57UYS9XGIk0HuWsAfYkL-fo4fEOJN8lQV",
-        label = "Add name ", --Target name 
+        webhook = "Add_webhook",
+        label = "Add_name", --Target name 
         roleId = "1262294980513366077" -- ID discord role 
     },
     {
         model = "s_f_y_scrubs_01", -- Ped model
         coords = vector4(-437.57, -323.18, 34.91, 164.03), -- Ped coords
-        webhook = "https://discord.com/api/webhooks/1283501957696196679/NvqSIgZID--oZAaXrm1OP0BEGqZQkXCJA-T7OQ9VczsX60oSmkEcWybFmWRKkosQ9ZzT",
-        label = "Add name ", --Target name 
-        roleId = "1259612267276927196" -- ID discord role 
+        webhook = "Add_webhook",
+        label = "Add_name", --Target name 
+        roleId = "1262294980513366077" -- ID discord role 
     },
     {
         model = "s_f_y_cop_01", -- Ped model
         coords = vector4(633.25, 8.85, 82.63, 165.19), -- Ped coords
-        webhook = "https://discord.com/api/webhooks/1283502868481048658/4DYQNZ5fmFxbrYNDvcIUTtUDH0Jzr05vtUZB8IBnKxmh9MStV7myOOnIUz6uXNcrT51a", 
-        label = "Add name ", --Target name 
-        roleId = "1259610372713873408" -- ID discord role 
+        webhook = "Add_webhook",
+        label = "Add_name", --Target name 
+        roleId = "1262294980513366077" -- ID discord role 
     },
     {
         model = "s_f_y_migrant_01", -- Ped model
         coords = vector4(2745.37, 3468.73, 55.67, 254.65), -- Ped coords
-        webhook = "https://discord.com/api/webhooks/1283503430412931073/j7nQES7df2ewfk5fcAnmgpAHX9O9fqoe9Tu8Q2A6-IhosRzVfwn7vriKSvs-uqVUXAjE",
-        label = "Add name ", --Target name 
-        roleId = "1262294295944237119" -- ID discord role 
+        webhook = "Add_webhook",
+        label = "Add_name", --Target name 
+        roleId = "1262294980513366077" -- ID discord role 
     },
     {
         model = "a_f_y_business_01", -- Ped model
         coords = vector4(-554.76, -185.18, 38.22, 217.66), -- Ped coords
-        webhook = "https://discord.com/api/webhooks/1283495196100919347/kV_IEV1GjfuOPipf5HmIV-S5iimHPVoI6UeHAJQolu0m9_Z3x4UD2cVV9rmdOkQzxeyE", 
-        label = "Add name ", --Target name 
-        roleId = "1262294978994896946" -- ID discord role 
+        webhook = "Add_webhook",
+        label = "Add_name", --Target name 
+        roleId = "1262294980513366077" -- ID discord role 
     }
 }
 
@@ -62,30 +63,60 @@ Citizen.CreateThread(function()
         exports['ox_target']:addLocalEntity(npc, {
             {
                 name = 'npc_talk_' .. _,
-                label = 'Talk ' .. npcData.label, -- label
+                label = 'Talk ' .. npcData.label,
                 icon = 'fas fa-comments',
                 onSelect = function()
-                    TriggerServerEvent('npc:requestPlayerInfo', npcData.webhook, npcData.roleId)
+                    TriggerEvent('npc:showProp', npc)
+                    StartMessageDialog(npcData.webhook, npcData.roleId)
                 end
             }
         })
     end
 end)
 
-RegisterNetEvent("npc:openMenuWithInfo")
-AddEventHandler("npc:openMenuWithInfo", function(charName, phoneNumber, webhook, roleId)
-    local input = exports.ox_lib:inputDialog("Leave a message", {
-        {label = "Leave your message here", type = "textarea"} 
+-- Функция за показване на проп
+RegisterNetEvent('npc:showProp')
+AddEventHandler('npc:showProp', function(npc)
+    local propModel = GetHashKey("prop_amb_phone") 
+    RequestModel(propModel)
+    while not HasModelLoaded(propModel) do
+        Wait(100)
+    end
+
+    local prop = CreateObject(propModel, GetEntityCoords(npc), true, true, true)
+    AttachEntityToEntity(prop, npc, GetPedBoneIndex(npc, 57005), 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, true, true, false, true, 1, true)
+end)
+
+function StartMessageDialog(webhook, roleId)
+    local input = exports.ox_lib:inputDialog("Оставете съобщение", {
+        {label = "Оставете вашето съобщение тук", type = "textarea"}
     })
 
     if input then
-        local additionalInfo = input[1] 
+        local additionalInfo = input[1]
         TriggerServerEvent("npc:sendToDiscord", {
-            charName = charName,
-            phoneNumber = phoneNumber,
+            charName = "Character Name", 
+            phoneNumber = "1234567890", 
             additionalInfo = additionalInfo,
             webhook = webhook,
             roleId = roleId
         })
+        QBCore.Functions.Notify("Съобщението беше изпратено успешно!")
+        
+
+        QBCore.Functions.Progressbar("progress_bar", "Обработка на информация...", 5000, false, true, {
+            disableMovement = true,
+            disableCarMovement = true,
+            disableMouse = false,
+            disableCombat = true,
+        }, {
+            animDict = "amb@world_human_clipboard@male@idle_a",
+            anim = "idle_a",
+            flags = 49,
+        }, {}, {}, function() 
+            QBCore.Functions.Notify("Операцията завърши успешно!")
+        end, function() 
+            QBCore.Functions.Notify("Операцията бе прекъсната!")
+        end)
     end
-end)
+end
